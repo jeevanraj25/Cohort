f03da9ab0975d02747a88a9a0246d8ea5542bd57
Jeevan <itsjeevanraj25@gmail.com>
2024-12-11 12:07:58 +0530

    Docker mounts

--- week_15/15.4/app/page.tsx
+++ week_15/15.4/app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            hello
           </a>
         </div>
       </main>
