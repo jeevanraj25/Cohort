ea0353efbd9973418328a2159e8ced0c8caf206d
Jeevan <itsjeevanraj25@gmail.com>
2024-12-17 13:15:48 +0530

    cohort made some changes

---
+++ week_15/15.6/src/db.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema, model } from 'mongoose';
+const mongoUrl = process.env.MONGO_URL || 'mongodb://mongodb:27017/myDatabase';
+
+
+// Connect to MongoDB
+const connectWithRetry = () => {
+  mongoose
+    .connect(mongoUrl)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => {
+      console.error('MongoDB connection error:', err);
+      setTimeout(connectWithRetry, 5000); // Retry after 5 seconds
+    });
+};
+
+connectWithRetry();
+
+// Define a User schema
+interface IUser {
+  name: string;
+  age: number;
+  email: string;
+}
+
+const UserSchema: Schema = new Schema<IUser>({
+  name: { type: String, required: true },
+  age: { type: Number, required: true },
+  email: { type: String, required: true }
+});
+
+// Create a User model
+export const User = model<IUser>('User', UserSchema);
