59444b8dc5643f1b3f40113540b4a8e09d0e5090
Jeevan <itsjeevanraj25@gmail.com>
2024-12-20 11:21:05 +0530

    wallet

---
+++ paytm-wallet/apps/user-app/app/lib/auth.ts
@@ -0,0 +1,73 @@
+import db from "@repo/db/client";
+import CredentialsProvider from "next-auth/providers/credentials"
+import bcrypt from "bcrypt";
+import GoogleProvider from "next-auth/providers/google";
+
+
+export const authOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID || '',
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || ''
+          }),
+      CredentialsProvider({
+          name: 'Credentials',
+          credentials: {
+            Email: { label: "Email", type: "text", placeholder: "enter your email", required: true },
+            password: { label: "Password", type: "password",placeholder: "enter your password", required: true }
+          },
+         
+          // TODO: User credentials type from next-aut
+          async authorize(credentials: any) {
+            // Do zod validation, OTP validation here
+            const hashedPassword = await bcrypt.hash(credentials.password, 10);
+            const existingUser = await db.user.findFirst({
+                where: {
+                    number: credentials.email
+                }
+            });
+
+            if (existingUser) {
+                const passwordValidation = await bcrypt.compare(credentials.password, existingUser.password);
+                if (passwordValidation) {
+                    return {
+                        id: existingUser.id.toString(),
+                        name: existingUser.name,
+                        email: existingUser.number
+                    }
+                }
+                return null;
+            }
+
+            try {
+                const user = await db.user.create({
+                    data: {
+                        number: credentials.phone,
+                        password: hashedPassword
+                    }
+                });
+            
+                return {
+                    id: user.id.toString(),
+                    name: user.name,
+                    email: user.number
+                }
+            } catch(e) {
+                console.error(e);
+            }
+
+            return null
+          },
+        })
+    ],
+    secret: process.env.JWT_SECRET || "secret",
+    callbacks: {
+        // TODO: can u fix the type here? Using any is bad
+        async session({ token, session }: any) {
+            session.user.id = token.sub
+
+            return session
+        }
+    }
+  }
+  
