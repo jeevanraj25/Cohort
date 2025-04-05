d131a4af67d1b6122832740980f8dd1279b45f6f
Jeevan <itsjeevanraj25@gmail.com>
2025-04-05 19:57:28 +0530

    Open-api

---
+++ week_20/hono-openapi/src/index.ts
@@ -0,0 +1,48 @@
+
+import { createRoute,OpenAPIHono  } from '@hono/zod-openapi'
+import { ParamsSchema } from './inputs'
+import { UserSchema } from './output'
+import { swaggerUI } from '@hono/swagger-ui'
+
+const route = createRoute({
+  method: 'get',
+  path: '/users/{id}',
+  request: {
+    params: ParamsSchema,
+  },
+  responses: {
+    200: {
+      content: {
+        'application/json': {
+          schema: UserSchema,
+        },
+      },
+      description: 'Retrieve the user',
+    },
+  },
+})
+
+const app = new OpenAPIHono()
+
+app.openapi(route, (c) => {
+  const { id } = c.req.valid('param')
+  return c.json({
+    id,
+    age: 20,
+    name: 'Ultra-man',
+  })
+})
+
+app.get('/ui', swaggerUI({ url: '/doc' }))
+
+// The OpenAPI documentation will be available at /doc
+app.doc('/doc', {
+  openapi: '3.0.0',
+  info: {
+    version: '1.0.0',
+    title: 'My API',
+  },
+})
+
+
+export default app
